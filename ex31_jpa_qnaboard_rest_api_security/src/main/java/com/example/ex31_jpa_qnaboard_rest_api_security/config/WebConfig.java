dd1b98999ff0919a3bfb84e0b148a5f39289193e
postforty <75767073+postforty@users.noreply.github.com>
2024-10-24 22:05:20 +0900

    CORS 해결

---
+++ ex31_jpa_qnaboard_rest_api_security/src/main/java/com/example/ex31_jpa_qnaboard_rest_api_security/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.ex31_jpa_qnaboard_rest_api_security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000") // React 앱의 URL
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowCredentials(true);
+  }
+}
